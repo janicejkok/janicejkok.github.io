d55f637b18f41f2d508a85a267f96eca88e762f9
Janice Jea Kok <30614134+janicejkok@users.noreply.github.com>
2019-05-16 02:07:49 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,6 +2,7 @@
 $to = "janicejkok@berkeley.edu";
 $subject = "Form submission";
 $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-$headers = "From: $_POST["email"]";
-mail($to,$subject,$message,$headers)
+$headers = "From: $_POST['email']";
+mail($to,$subject,$message,$headers);
+  echo "Message Sent.";
 ?>
