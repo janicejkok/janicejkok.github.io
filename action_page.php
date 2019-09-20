0bbab33f5139409b41c0556c9f33aa98f03c4e40
Janice Jea Kok <30614134+janicejkok@users.noreply.github.com>
2019-09-20 23:16:59 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,8 +1,18 @@
-<?php
-$to = "janicejkok@berkeley.edu";
-$subject = "Form submission";
-$message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-$headers = "From: $_POST['email']";
-mail($to,$subject,$message,$headers);
-  echo "Message Sent.";
+<?php 
+if(isset($_POST['submit'])){
+    $to = "janicejkok@berkeley.edu";
+    $from = $_POST['email']; 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); 
+    echo "Your message has been sent. Thank you " . $first_name . ", I will contact you shortly.";
+    }
 ?>
