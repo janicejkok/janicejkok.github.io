281bdf4e21c3f7a747e3c638a98733f251e1983c
Janice Jea Kok <30614134+janicejkok@users.noreply.github.com>
2019-09-22 02:24:55 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,24 +1,8 @@
-
-<?php 
-
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
-if(isset($_POST['submit'])){
-    $to = "janicejkok@berkeley.edu";
-    $from = $_POST['email']; 
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); 
-    echo "Your message has been sent. Thank you " . $first_name . ", I will contact you shortly.";
-    }
+<?php
+$to = "janicejkok@berkeley.edu";
+$subject = "Form submission";
+$message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+$headers = "From: $_POST['email']";
+mail($to,$subject,$message,$headers);
+  echo "Message Sent.";
 ?>
